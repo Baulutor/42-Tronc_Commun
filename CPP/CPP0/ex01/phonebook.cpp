134fc9d3547451c61b4c54fb9209d25b2e2dae01
Baulutor <dbaule@z2r8p2.42lyon.fr>
2024-01-23 13:30:44 +0100

    netpractice done

---
+++ CPP/CPP0/ex01/phonebook.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dbaule on 1/22/24.
+//
+
+#include "phonebook.h"
---
+++ CPP/CPP0/ex01/phonebook.h
@@ -0,0 +1,14 @@
+//
+// Created by dbaule on 1/22/24.
+//
+
+#ifndef CPP_PHONEBOOK_H
+#define CPP_PHONEBOOK_H
+
+
+class phonebook {
+
+};
+
+
+#endif //CPP_PHONEBOOK_H
